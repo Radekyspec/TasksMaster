31e3510d635a4acf0af3c3e075f22eb89424f10e
Radekyspec <93813543+Radekyspec@users.noreply.github.com>
2023-10-30 11:31:53 -0400

    Create branch and initial main (#1)

---
+++ src/app/Main.java
@@ -0,0 +1,21 @@
+package app;
+
+import interface_adapter.ViewManagerModel;
+import view.ViewManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame application = new JFrame("TasksMaster");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(cardLayout, views, viewManagerModel);
+    }
+}
