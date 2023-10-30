3679b29cc98335b4e5baeddcc9792ad62d83fbdb
liang460 <yukai.liang@mail.utoronto.ca>
2023-10-30 14:18:08 -0400

    Add login and signup use case interface

---
+++ src/use_case/signup/SignupOutputBoundary.java
@@ -0,0 +1,6 @@
+package use_case.signup;
+
+public interface SignupOutputBoundary {
+    void prepareSuccessView(SignupOutputData signupOutputData);
+    void prepareFailView(SignupOutputData signupOutputData);
+}
