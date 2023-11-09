02e492f908c091c47c361d67fd4db919792f7131
zfcl9 <sdfjfwefj@mail.ccc>
2023-11-08 23:30:26 -0500

    IV

--- src/entities/todo_list/CommonToDoList.java
+++ src/entities/todo_list/CommonToDoList.java
@@ -1,19 +1,17 @@
 package entities.todo_list;
 
 import entities.todo.ToDo;
-
 import java.util.Map;
+import java.util.HashMap;
 
 public class CommonToDoList implements ToDoList{
     private final Integer ID;
-    private final Integer LastID;
     private String name;
     private String detail;
     private Map<Integer, ToDo> toDos;
 
     public CommonToDoList(Integer LastID, String name, String detail,
                           Map<Integer, ToDo> toDos){
-        this.LastID = LastID;
         this.ID = LastID + 1; // No detect of ID < 0
         this.name = name;
         this.detail = detail;
@@ -21,7 +19,12 @@ public class CommonToDoList implements ToDoList{
     }
     
     @Override
-    public getToDoList(){
-        return [ID, name, detail, toDos]; 
+    public Map<String, Object> getToDoList(){
+        Map<String, Object> tdlMap = new HashMap<>();
+        tdlMap.put("ID", ID);
+        tdlMap.put("name", name);
+        tdlMap.put("detail", detail);
+        tdlMap.put("toDos", toDos);
+        return tdlMap;
     }
 }
