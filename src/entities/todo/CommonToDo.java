30c4bce363d9856c95524ccd408591b46e933b0c
zfcl9 <sdfjfwefj@mail.ccc>
2023-11-08 23:27:57 -0500

    III Merged into Main 11082200 to prevent conflict.

--- src/entities/todo/CommonToDo.java
+++ src/entities/todo/CommonToDo.java
@@ -1,27 +1,31 @@
 package entities.todo;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommonToDo implements ToDo{
     private final Integer ID; // No one will pass an id inside, create a unique id once it created
     // Is Integer type proper here when facing large id?
-    private final Integer LastID; 
     private String target;
     private String[] assignedTo;
-    // Do we need to make it all final here? Do we need to change it afterwards? 
+    // Do we need to make it all final here? Do we need to change it afterward?
 
-    public CommonToDo(Integer LastID, String target, String[] assignedTo){ // No expection of id pass in.
+    public CommonToDo(Integer LastID, String target, String[] assignedTo){ // No expectation of id pass in.
         // This is a setter. 
-        this.LastID = LastID;
         this.target = target;
         this.assignedTo = assignedTo;
         this.ID = LastID + 1;
         // how to check repeated id? by a method inside here.
         // We should have a db that store our data.
-        // We store data temporaryly in a text file.
+        // We store data temporarily in a text file.
     }
     
     @Override
-    public getToDo(){
-        return [ID, target, assignedTo];
+    public Map<String, Object> getToDo(){
+        Map<String, Object> tdM = new HashMap<>();
+        tdM.put("ID", ID);
+        tdM.put("target", target);
+        tdM.put("assignedTo", assignedTo);
+        return tdM;
     }
 }
