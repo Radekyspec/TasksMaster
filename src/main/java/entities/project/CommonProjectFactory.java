282912ebc3352ccfbe2ba0fc66239f5cb435fa73
Radekyspec <w41k3r15347@gmail.com>
2023-11-28 04:47:47 -0500

    update comments

--- src/main/java/entities/project/CommonProjectFactory.java
+++ src/main/java/entities/project/CommonProjectFactory.java
@@ -5,6 +5,7 @@ public class CommonProjectFactory {
      * Create a CommonProject object.
      * @param ID the ID of the project
      * @param name the name of the project
+     * @param description the description of the project
      */
     public static CommonProject create(int ID, String name, String description){
         return new CommonProject(ID, name, description);
