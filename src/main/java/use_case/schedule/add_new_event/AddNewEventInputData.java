4593b09ffbf636db16c566d373f64b05bc724174
tonyycdd <42673735+tonyycdd@users.noreply.github.com>
2023-12-02 00:11:38 -0500

    change event user_case and interface_adapter

--- src/main/java/use_case/schedule/add_new_event/AddNewEventInputData.java
+++ src/main/java/use_case/schedule/add_new_event/AddNewEventInputData.java
@@ -2,8 +2,59 @@ package use_case.schedule.add_new_event;
 
 import entities.user.User;
 
+import java.util.Date;
+import java.util.List;
+
 public class AddNewEventInputData {
     private int projectId;
-    private int ScheduleId;
+    private int scheduleId;
     private String eventName;
+    private String notes;
+    private Date startAt;
+    private Date endAt;
+    private boolean isAllDay;
+    private List<User> userWith;
+
+    public AddNewEventInputData(int projectId, int scheduleId, String eventName, String notes, Date startAt, Date endAt, boolean isAllDay, List<User> userWith) {
+        this.projectId = projectId;
+        this.scheduleId = scheduleId;
+        this.eventName = eventName;
+        this.notes = notes;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.isAllDay = isAllDay;
+        this.userWith = userWith;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public int getScheduleId() {
+        return scheduleId;
+    }
+
+    public String getEventname() {
+        return eventName;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public Date getStartAt() {
+        return startAt;
+    }
+
+    public Date getEndAt() {
+        return endAt;
+    }
+
+    public boolean isAllDay() {
+        return isAllDay;
+    }
+
+    public List<User> getUserWith() {
+        return userWith;
+    }
 }
--- src/main/java/view/schedule/ScheduleView.java
+++ src/main/java/view/schedule/ScheduleView.java
@@ -1,32 +1,39 @@
 package view.schedule;
 
-import entities.event.Event;
 import entities.user.User;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.schedule.ScheduleController;
-import interface_adapter.schedule.SchedulePresenter;
-import interface_adapter.schedule.ScheduleState;
 import interface_adapter.schedule.ScheduleViewModel;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 public class ScheduleView extends JPanel implements ActionListener, PropertyChangeListener{
     private User user;
     private final ViewManagerModel viewManagerModel;
-    private final EventViewModel eventViewModel;
+    private final ScheduleViewModel scheduleViewModel;
+    //private final EventViewModel eventViewModel;
+    //private final AddNewEventViewModel addNewEventViewModel;
+    private final ScheduleController scheduleController;
+    private final JButton addNewEvent;
+    private final JPanel
     @Override
     public void actionPerformed(ActionEvent e) {
-
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
+        //
+    }
 
+    public ScheduleView(User user, ViewManagerModel viewManagerModel, ScheduleViewModel scheduleViewModel, ScheduleController scheduleController, JButton addNewEvent) {
+        this.user = user;
+        this.viewManagerModel = viewManagerModel;
+        this.scheduleViewModel = scheduleViewModel;
+        this.scheduleController = scheduleController;
+        this.addNewEvent = addNewEvent;
     }
 }
