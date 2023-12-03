45a2222e4b88d0024e894ae121c4c88014ba4412
tonyycdd <42673735+tonyycdd@users.noreply.github.com>
2023-12-02 23:59:43 -0500

    change schedule&event user_case and interface_adapter

--- src/main/java/interface_adapter/schedule/ScheduleController.java
+++ src/main/java/interface_adapter/schedule/ScheduleController.java
@@ -14,8 +14,8 @@ public class ScheduleController {
         this.scheduleInteractor = scheduleInteractor;
     }
 
-    public void getEvent(long projectId, long eventId, String name, String note, Date startAt, Date endAt, boolean isAllDay, List<String> userWith) {
-        ScheduleInputData scheduleInputData = new ScheduleInputData(projectId, eventId, name, note, startAt, endAt, isAllDay, userWith);
+    public void getEvent(long projectId, long eventId) {
+        ScheduleInputData scheduleInputData = new ScheduleInputData(projectId, eventId,null,null,null,null,null,null);
         scheduleInteractor.getEvent(scheduleInputData);
     }
 
