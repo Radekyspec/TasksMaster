71e3b320fed68471bf9c33fa8a3380f2792306d8
tonyycdd <42673735+tonyycdd@users.noreply.github.com>
2023-12-02 21:25:54 -0500

    change schedule&event user_case and interface_adapter

--- src/main/java/view/schedule/ScheduleView.java
+++ src/main/java/view/schedule/ScheduleView.java
@@ -7,6 +7,8 @@ import interface_adapter.ViewManagerModel;
 import interface_adapter.schedule.ScheduleViewModel;
 import interface_adapter.schedule.ScheduleController;
 import interface_adapter.schedule.ScheduleState;
+import interface_adapter.schedule.event.AddEventState;
+import interface_adapter.schedule.event.AddEventViewModel;
 
 import javax.swing.*;
 import java.util.Calendar;
@@ -26,20 +28,45 @@ public class ScheduleView extends JPanel implements ActionListener, PropertyChan
     private User user;
     private final ViewManagerModel viewManagerModel;
     private final ScheduleViewModel scheduleViewModel;
-    //private final EventViewModel eventViewModel;
-    //private final AddNewEventViewModel addNewEventViewModel;
+    private final AddEventViewModel addEventViewModel;
     private final ScheduleController scheduleController;
     private final JButton addNewEvent;
-    private final JPanel addEventPanel = new JPanel();
-    //private final JPanel;
+    private final JPanel event;
+    private final JPanel addEventPanel;
+
+    public ScheduleView(ViewManagerModel viewManagerModel, ScheduleViewModel scheduleViewModel, AddEventViewModel addEventViewModel, ScheduleController scheduleController) {
+        this.viewManagerModel = viewManagerModel;
+        this.scheduleViewModel = scheduleViewModel;
+        this.addEventViewModel = addEventViewModel;
+        this.scheduleController = scheduleController;
+        scheduleViewModel.addPropertyChangeListener(this);
+
+        addNewEvent = new JButton(ScheduleViewModel.SCHEDULE_ADD_NEW_EVENT);
+        addNewEvent.addActionListener(
+                e -> {
+                    if (!e.getSource().equals(addNewEvent)) {
+                        return;
+                    }
+                    viewManagerModel.setActiveView(addEventViewModel.getViewName());
+                    AddEventState addEventState = addEventViewModel.getAddEventState();
+                    addEventState.setProjectId(scheduleViewModel.getScheduleState().getProjectId());
+                    addEventState.setScheduleId(scheduleViewModel.getScheduleState().getScheduleId());
+                    addEventState.setStartAt();
+                }
+        );
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-
+        ScheduleState state = (ScheduleState) evt.getNewValue();
+        switch (evt.getPropertyName()) {
+            case ScheduleViewModel.
+        }
     }
 
     public ScheduleView(User user, ViewManagerModel viewManagerModel, ScheduleViewModel scheduleViewModel, ScheduleController scheduleController, JButton addNewEvent) {
